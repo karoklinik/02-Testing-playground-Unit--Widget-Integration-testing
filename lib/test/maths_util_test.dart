bfd691724ce60d35add43e3c382e085fde64b7fb
Karolina Klinik <56517458+karoklinik@users.noreply.github.com>
2023-08-28 21:40:14 +0200

    More unit tests & grouping them
    9. Unit test in more details & grouping of unit tests

--- lib/test/maths_util_test.dart
+++ lib/test/maths_util_test.dart
@@ -2,15 +2,41 @@ import 'package:test/test.dart';
 import 'package:testing_course_udemy/maths_util.dart';
 
 void main() {
-  test('check if adding works correct', () {
-    //arrange
-    const a = 30;
-    var b = 10;
+  group('maths functions testing - ', () {
+    test('check if adding works correct', () {
+      //arrange
+      const a = 30;
+      var b = 10;
 
-    //act
-    var sum = add(a, b);
+      //act
+      var sum = add(a, b);
 
-    //assert
-    expect(sum, 40);
+      //assert
+      expect(sum, 40);
+    });
+
+    test('check if substraction works correct', () {
+      //arrange
+      const a = 30;
+      var b = 10;
+
+      //act
+      var sum = substract(a, b);
+
+      //assert
+      expect(sum, 20);
+    });
+
+    test('check if multiply works correct', () {
+      //arrange
+      const a = 30;
+      var b = 10;
+
+      //act
+      var result = multiply(a, b);
+
+      //assert
+      expect(result, 300);
+    });
   });
 }
